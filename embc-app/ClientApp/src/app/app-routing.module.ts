e35e75bab4be9bac7535d148ceea86d075bbd9d4
Alejandro Sanchez <emailforasr@gmail.com>
2019-02-11 15:30:12 -0800

    Code cleanup

--- embc-app/ClientApp/src/app/app-routing.module.ts
+++ embc-app/ClientApp/src/app/app-routing.module.ts
@@ -1,39 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BusinessProfileComponent } from './business-information/business-profile/business-profile.component';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { CanDeactivateGuard } from './services/can-deactivate-guard.service';
-import { BCeidAuthGuard } from './services/bceid-auth-guard.service';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProfileSummaryComponent } from './business-information/profile-summary/profile-summary.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'business-profile',
-    component: BusinessProfileComponent,
-    canActivate: [BCeidAuthGuard]
-  },
-  {
-    path: 'business-profile-review',
-    component: ProfileSummaryComponent,
-    canActivate: [BCeidAuthGuard]
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [BCeidAuthGuard]
-  },
-  { path: '**', component: NotFoundComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
-  exports: [RouterModule],
-  providers: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- embc-app/ClientApp/src/app/models/business-contact.model.ts
+++
@@ -1,8 +0,0 @@
-import { DynamicsContact } from './dynamics-contact.model';
-
-export class BusinessContact {
-  id: string;
-  jobTitle: string;
-  contactType: string;
-  contact: DynamicsContact;
-}
